1fd02fc89e9d83ae562f2d77b94977e8e2804154
Edward Reyes <reyes.edwardf@gmail.com>
2020-08-01 01:26:30 -0700

    success: saved events to local storage and render them to page when reload

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,57 @@
-$("textarea").on("click", function () {
-    // Only concatenate firstNumber while operator has not been pressed
-    console.log($(this).val());
-})
+var events = []; // To store events
+
+
+// $("textarea").on("click", function () {
+//     // Only concatenate firstNumber while operator has not been pressed
+//     console.log($(this).val());
+// })
+function initEvents() {
+    var storedEvents = JSON.parse(localStorage.getItem("events"));
+    // If there are no events, exit function
+    if (storedEvents === null) {
+        return;
+    }
+    
+    storedEvents.forEach(element => events.push(element));
+    renderEvents();
+    // $("textarea[data-hour*='" + hour + "']").addClass("present");
+}
+initEvents();
+
+function renderEvents() {
+    if (events.length === 0) {
+        return;
+    }
+    // render events to each appropriate time-block
+    for (var i = 0; i < events.length; i++) {
+        var textarea = $("textarea[data-hour*='" + events[i].dataHour + "']");
+        textarea[0].textContent = events[i].description;
+    }
+}
 
 $("button").on("click", function () {
-    // Only concatenate firstNumber while operator has not been pressed
-    console.log($(this).prev()[0].value);
+    // Get the previous sibling
+    var textarea = $(this).prev()[0]; 
+
+    // Get the data-hour attribute
+    var dataHour = textarea.getAttribute('data-hour')
+    var description = textarea.value.trim();
+
+    // If event is empty, exit function
+    if (description.length === 0) {
+        alert("Event empty");
+        return;
+    }
+
+    var event = {
+        description: description,
+        dataHour: dataHour
+    }
+
+    events.push(event);
+
+    localStorage.setItem("events", JSON.stringify(events))
+    
+
 })
 
